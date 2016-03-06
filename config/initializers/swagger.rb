bed60c4771634371f31a1a4e94d283c611a1831b
elzzup <guild0105@gmail.com>
2016-03-07 06:12:44 +0900

    DON!

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,3 +1,3 @@
 GrapeSwaggerRails.options.url      = 'swagger_doc'
-GrapeSwaggerRails.options.app_name = 'Don'
+GrapeSwaggerRails.options.app_name = 'DisasterMeshAPI'
 GrapeSwaggerRails.options.app_url  = '/api/v1/'
