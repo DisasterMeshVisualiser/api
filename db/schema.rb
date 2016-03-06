fa8f81345cbe7882d073795c2a7812f2bd008ca5
elzzup <guild0105@gmail.com>
2016-03-07 00:28:34 +0900

    Sync schema :sorry:

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306150740) do
+ActiveRecord::Schema.define(version: 20160306152621) do
 
   create_table "mesh_codes", force: :cascade do |t|
     t.string   "value"
     t.integer  "rank"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.float    "southwest_lat"
+    t.float    "southwest_long"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "mesh_types", force: :cascade do |t|
