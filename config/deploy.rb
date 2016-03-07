9dabdbd70e0ce875fe1d20f4b162bea56d08480c
elzzup <guild0105@gmail.com>
2016-03-07 18:58:39 +0900

    Update deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,48 +11,19 @@ set :log_level, :debug
 set :pty, true
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets bundle public/system public/assets}
 set :default_env, { path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH" }
-#5回分のreleasesを保持する
 set :keep_releases, 5
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+namespace :deploy do #タスクnamespace
+  #desc 'タスク説明'
+  #task :restart do #タスク定義
+  #ここにタスク処理内容を記述
+  #end
+  #after :finishing, 'deploy:cleanup' #task実行タイミングを指定できます。詳細は下記
+  #http://capistranorb.com/documentation/getting-started/flow/
+  #サンプルにunicorn再起動タスク
+  desc 'Restart application'
+  task :restart do
+    invoke 'unicorn:restart'
   end
-
+  after :finishing, 'deploy:cleanup'
 end
