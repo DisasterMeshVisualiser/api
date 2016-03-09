f6cf7c7a69b7bab068e33a44869769e9517fb9cf
elzzup <guild0105@gmail.com>
2016-03-09 13:55:48 +0900

    Sit Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160306152621) do
+ActiveRecord::Schema.define(version: 20160306152622) do
 
   create_table "mesh_codes", force: :cascade do |t|
     t.string   "value"
